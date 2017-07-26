3e46354cfec1a3f5e7a0ec2e0ff9124b8bf9347a
P. Gomersbach <p.gomersbach@rely.nl>
2017-07-26 08:51:55 +0000

    add spec tests

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -16,11 +16,13 @@ describe 'profile_nfs' do
 
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to contain_class('profile_nfs') }
-
-          it { is_expected.to contain_class('profile_nfs') }
+          it { is_expected.to contain_class('profile_nfs::params') }
 
           it { is_expected.to contain_nfs__server__export('/mnt/nfs') }
-  
+          it { is_expected.to contain_nfs__server__export('/mnt/nfs/config') }
+          it { is_expected.to contain_nfs__server__export('/mnt/nfs/errors') }
+          it { is_expected.to contain_nfs__server__export('/mnt/nfs/logs') }
+          it { is_expected.to contain_nfs__server__export('/mnt/nfs/office-templates') }
 
         end
       end
